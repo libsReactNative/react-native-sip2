a462555ec4bacd33927478f6899c5b551dd0b39a
Anton Dodonov <anton.v.dodonov@gmail.com>
2025-06-28 17:23:39 +0700

    Update to react-native 0.80 and gradle 8.14.2

---
+++ examples/android/app/src/release/java/one/telefon/sip2example/ReactNativeFlipper.java
@@ -0,0 +1,14 @@
+package one.telefon.sip2example;
+
+import android.content.Context;
+import com.facebook.react.ReactInstanceManager;
+
+/**
+ * Class responsible for loading Flipper inside your React Native application. This is the release
+ * flavor of this class.
+ */
+public class ReactNativeFlipper {
+  public static void initializeFlipper(Context context, ReactInstanceManager reactInstanceManager) {
+    // Do nothing as we don't want to initialize Flipper on Release.
+  }
+} 
